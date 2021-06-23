5ac5dcaeabf08e96f96a276a58e059d0ebf91c5d
zlerp <zhpdeals@gmail.com>
2021-06-23 03:59:42 -0400

    create inital layout, header, banner-slider, and features sections

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false
+});
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 const autoprefixer = require('gulp-autoprefixer');
 
 
@@ -11,7 +12,8 @@ gulp.task('sass', function () {
     return gulp.src('./assets/css/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer('last 2 versions'))
-        .pipe(gulp.dest('./assets/css'));
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(livereload());;
 });
 
 gulp.task('sass:watch', async function () {
@@ -22,8 +24,13 @@ gulp.task('minify-css', function() {
     return gulp.src('./assets/css/style.css')
         .pipe(cleanCSS())
         .pipe(rename('dist.min.css'))
-        .pipe(gulp.dest('./assets/css/dist'));
+        .pipe(gulp.dest('./assets/css/dist'))
+        .pipe(livereload());
 });
 
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('./assets/*.scss', ['sass']);
+});
 
 gulp.task('default', gulp.series('sass', 'minify-css', 'sass:watch'));
